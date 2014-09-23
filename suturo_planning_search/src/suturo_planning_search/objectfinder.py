2f279f0d69553b58b2e88fd1265d6503d219fe89
Jannik Buckelo <jannikbu@tzi.de>
2014-09-23 19:54:42 +0200

    Added geometry stuff for object finder

--- suturo_planning_search/src/suturo_planning_search/objectfinder.py
+++ suturo_planning_search/src/suturo_planning_search/objectfinder.py
@@ -1,4 +1,5 @@
 import rospy
+import math
 from searchgrid import SearchGrid
 from moveit_msgs.srv import GetPlanningScene
 from moveit_msgs.msg import PlanningSceneComponents
@@ -8,14 +9,19 @@ class ObjectFinder:
 
     _grid = None
     _collision_objects = []
+    _fov_h = 0
+    _fov_v = 0
 
     def __init__(self, yaml):
         self._grid = SearchGrid(50, 50)
+        camera = yaml.sensors[0].camera
+        self._fov_h = camera.horizontal_fov
+        self._fov_v = 2.0 * math.atan(math.tan(self._fov_h / 2.0) * (camera.image_width / camera.image_height))
 
     def update_search_grid(self):
         # TODO Get planning scene from moveit service
         # TODO Get gripper pose and calculate visible fields
-        None
+       None
 
     def get_place_to_search(self):
         None
