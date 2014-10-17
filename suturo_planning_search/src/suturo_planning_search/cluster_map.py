d947eec80edc515e77a299d4aa5d91e218068c0a
pmania <pmania@battlestation.local>
2014-10-17 17:37:08 +0200

    added segment_id to Cell. Changed ClusterRegions to cluster every different type of Cell

--- suturo_planning_search/src/suturo_planning_search/cluster_map.py
+++ suturo_planning_search/src/suturo_planning_search/cluster_map.py
@@ -18,8 +18,11 @@ def test_field1():
 
 class Segment:
     id = 0
-    center_coords = [0, 0]
 
+class RegionType:
+    obstacles = 1
+    unknown = 2
+    free = 3
 
 class ClusterRegions:
     ''' This class takes a field, which is represented as a 2D-array.
@@ -27,11 +30,28 @@ class ClusterRegions:
         where you should label every cell with SEGMENT_MAP_NOT_COLORED or
         SEGMENT_COLORED_FIELD, depending on which fields in your 2d array should be
         grouped.
+
+        Usage:
+        cm = ClusterRegions()
+        cm.set_field(YOUR_2D_ARRAY_WITH_CELL_INSTANCES)
+        print "Region count: " + str(cm.group_regions())
+        updated_map = cm.get_result_map()
     '''
     SEGMENT_MAP_NOT_COLORED = 0 # cells that should not be grouped
     SEGMENT_COLORED_FIELD = 1 # cells that should be grouped (for example obstacles)
 
     map_width = 25
+    segments = []
+    result_field = [[]]
+
+    # Group unknown fields as default
+    type = RegionType.unknown
+
+    def set_region_type(self, type):
+        '''
+        Specify, which of the fields in a Cell should be grouped together as regions(unknown, obstacles, free)
+        '''
+        self.type = type
 
     def init_field(self):
         c = Cell()
@@ -39,6 +59,9 @@ class ClusterRegions:
         self.field = [[c for x in xrange(self.map_width)] for x in xrange(self.map_width)]
         self.segmented_field = [[self.SEGMENT_MAP_NOT_COLORED for x in xrange(self.map_width)] for x in
                                 xrange(self.map_width)]
+
+    def init_segment_list(self):
+        self.segments = []
 
     def __init__(self):
         self.init_field()
@@ -73,6 +96,9 @@ class ClusterRegions:
 
         # Color the field with the desired color
         self.segmented_field[x][y] = color
+        self.result_field = self.field
+        self.result_field[x][y].segment_id = color
+
         # Color the adjacent cells
         if (x > 0): self.fill_cell(x - 1, y, color)
         if (x < len(self.segmented_field) - 1): self.fill_cell(x + 1, y, color)
@@ -83,10 +109,19 @@ class ClusterRegions:
         ''' Turn the current self.field into a binary region map, where self.group_regions() can be performed '''
         for x in xrange(len(self.field)):
             for y in xrange(len(self.field[0])):
-                if self.field[x][y].is_free():
-                    self.segmented_field[x][y] = self.SEGMENT_MAP_NOT_COLORED
-                else:
+                self.segmented_field[x][y] = self.SEGMENT_MAP_NOT_COLORED
+
+                # Color the cell depending on the desired region type
+                if self.type == RegionType.unknown and self.field[x][y].is_unknown():
                     self.segmented_field[x][y] = self.SEGMENT_COLORED_FIELD
+
+                if self.type == RegionType.obstacles and self.field[x][y].is_obstacle():
+                    self.segmented_field[x][y] = self.SEGMENT_COLORED_FIELD
+
+                if self.type == RegionType.free and self.field[x][y].is_free():
+                    self.segmented_field[x][y] = self.SEGMENT_COLORED_FIELD
+
+
 
     def group_regions(self):
         self.convert_field_to_region_map()
@@ -105,10 +140,15 @@ class ClusterRegions:
                     region_color += 1
         return region_color - start_color
 
+    def get_result_map(self):
+        return self.result_field
+
 
 cm = ClusterRegions()
 cm.set_field(test_field1())
 cm.print_field()
-cm.group_regions()
+
+print "Returned regions: " + str(cm.group_regions())
 print "Grouping done"
 cm.print_segmented_field()
+c = cm.get_result_map()
