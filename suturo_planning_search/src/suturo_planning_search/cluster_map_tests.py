e16710290491df066eae5218e38ea0580bb491c9
pmania <pmania@battlestation.local>
2014-10-21 17:29:39 +0200

    more pyunit tests <3

--- suturo_planning_search/src/suturo_planning_search/cluster_map_tests.py
+++ suturo_planning_search/src/suturo_planning_search/cluster_map_tests.py
@@ -61,6 +61,12 @@ class TestRegionMerge(unittest.TestCase):
         self.assertEqual(r.max_y,1)
 
     def test_conditional_region_merge(self):
+        """ Two blocks below each other. Merge should work
+        xx
+        xx
+        xx
+        xx
+        """
         r = Region(1)
         r2 = Region(2)
 
@@ -85,11 +91,160 @@ class TestRegionMerge(unittest.TestCase):
         r2.max_x = 3
         r2.max_y = 1
 
-        r.merge_region(r2)
+        self.assertTrue(r.merge_region_w_criteria(1,1,r2))
 
         # Check the size of cell lists
         self.assertTrue(len(r.cells) == 8)
         self.assertTrue(len(r.cell_coords) == 8)
+
+        self.assertTrue([0,0] in r.cell_coords)
+        self.assertTrue([0,1] in r.cell_coords)
+        self.assertTrue([1,0] in r.cell_coords)
+        self.assertTrue([1,1] in r.cell_coords)
+        self.assertTrue([2,0] in r.cell_coords)
+        self.assertTrue([2,1] in r.cell_coords)
+        self.assertTrue([3,0] in r.cell_coords)
+        self.assertTrue([3,1] in r.cell_coords)
+
+        self.assertEqual(r.min_x,0)
+        self.assertEqual(r.min_y,0)
+        self.assertEqual(r.max_x,3)
+        self.assertEqual(r.max_y,1)
+    def test_conditional_region_merge_2(self):
+        """ Two blocks besides and below each other. Shouldn't be merged.
+            xx
+            xx
+              xx
+              xx
+        """
+        r = Region(1)
+        r2 = Region(2)
+
+        r.cells = [Cell() for x in xrange(4)]
+        r.cell_coords.append([0,0])
+        r.cell_coords.append([0,1])
+        r.cell_coords.append([1,0])
+        r.cell_coords.append([1,1])
+        r.min_x = 0
+        r.min_y = 0
+        r.max_x = 1
+        r.max_y = 1
+
+
+        r2.cells = [Cell() for x in xrange(4)]
+        r.cell_coords.append([2,2])
+        r.cell_coords.append([2,3])
+        r.cell_coords.append([3,2])
+        r.cell_coords.append([3,3])
+        r2.min_x = 2
+        r2.min_y = 2
+        r2.max_x = 3
+        r2.max_y = 3
+
+        self.assertFalse(r.merge_region_w_criteria(1,1,r2))
+
+        # Check the size of cell lists
+        self.assertTrue(len(r.cells) == 4)
+        self.assertTrue(len(r.cell_coords) == 4)
+
+    def test_conditional_region_merge3(self):
+        """ Two blocks diagonal each other. Merge should work
+        xx
+         xx
+        """
+        r = Region(1)
+        r2 = Region(2)
+
+        r.cells = [Cell() for x in xrange(4)]
+        r.cell_coords.append([0,0])
+        r.cell_coords.append([0,1])
+        r.cell_coords.append([1,0])
+        r.cell_coords.append([1,1])
+        r.min_x = 0
+        r.min_y = 0
+        r.max_x = 1
+        r.max_y = 1
+
+
+        r2.cells = [Cell() for x in xrange(4)]
+        r.cell_coords.append([2,1])
+        r.cell_coords.append([2,2])
+        r.cell_coords.append([3,1])
+        r.cell_coords.append([3,2])
+        r2.min_x = 2
+        r2.min_y = 1
+        r2.max_x = 3
+        r2.max_y = 2
+
+        self.assertTrue(r.merge_region_w_criteria(1,1,r2))
+
+        # Check the size of cell lists
+        self.assertTrue(len(r.cells) == 8)
+        self.assertTrue(len(r.cell_coords) == 8)
+
+        self.assertTrue([0,0] in r.cell_coords)
+        self.assertTrue([0,1] in r.cell_coords)
+        self.assertTrue([1,0] in r.cell_coords)
+        self.assertTrue([1,1] in r.cell_coords)
+        self.assertTrue([2,1] in r.cell_coords)
+        self.assertTrue([2,2] in r.cell_coords)
+        self.assertTrue([3,1] in r.cell_coords)
+        self.assertTrue([3,2] in r.cell_coords)
+
+        self.assertEqual(r.min_x,0)
+        self.assertEqual(r.min_y,0)
+        self.assertEqual(r.max_x,3)
+        self.assertEqual(r.max_y,2)
+
+    def test_conditional_region_merge4(self):
+        """ Two blocks diagonal of each other. But this time, no left/right tolerance in merge is given.
+         Merge should not work
+        xx
+         xx
+        """
+        r = Region(1)
+        r2 = Region(2)
+
+        r.cells = [Cell() for x in xrange(4)]
+        r.cell_coords.append([0,0])
+        r.cell_coords.append([0,1])
+        r.cell_coords.append([1,0])
+        r.cell_coords.append([1,1])
+        r.min_x = 0
+        r.min_y = 0
+        r.max_x = 1
+        r.max_y = 1
+
+
+        r2.cells = [Cell() for x in xrange(4)]
+        r.cell_coords.append([2,1])
+        r.cell_coords.append([2,2])
+        r.cell_coords.append([3,1])
+        r.cell_coords.append([3,2])
+        r2.min_x = 2
+        r2.min_y = 1
+        r2.max_x = 3
+        r2.max_y = 2
+
+        self.assertFalse(r.merge_region_w_criteria(0,0,r2))
+
+        # Check the size of cell lists
+        self.assertTrue(len(r.cells) == 8)
+        self.assertTrue(len(r.cell_coords) == 8)
+
+        self.assertTrue([0,0] in r.cell_coords)
+        self.assertTrue([0,1] in r.cell_coords)
+        self.assertTrue([1,0] in r.cell_coords)
+        self.assertTrue([1,1] in r.cell_coords)
+        self.assertTrue([2,1] in r.cell_coords)
+        self.assertTrue([2,2] in r.cell_coords)
+        self.assertTrue([3,1] in r.cell_coords)
+        self.assertTrue([3,2] in r.cell_coords)
+
+        self.assertEqual(r.min_x,0)
+        self.assertEqual(r.min_y,0)
+        self.assertEqual(r.max_x,3)
+        self.assertEqual(r.max_y,2)
 
 
 if __name__ == '__main__':
