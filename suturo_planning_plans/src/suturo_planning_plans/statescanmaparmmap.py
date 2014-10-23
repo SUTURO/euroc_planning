efa15e5fccdebcae6c0b65b5a5ba911408c1ac4f
andz <aromero@uni-bremen.de>
2014-10-23 20:36:04 +0200

    Removed unused imports!!!!1111

--- suturo_planning_plans/src/suturo_planning_plans/statescanmaparmmap.py
+++ suturo_planning_plans/src/suturo_planning_plans/statescanmaparmmap.py
@@ -1,17 +1,11 @@
-from OpenSSL import rand
 from random import randint
-from geometry_msgs.msg._Point import Point
 import smach
 import rospy
-from geometry_msgs.msg import PoseStamped
 from suturo_planning_manipulation.mathemagie import *
 from suturo_planning_manipulation.calc_grasp_position import make_scan_pose, make_grasp_pose
-from suturo_planning_search.map import Map
 from suturo_planning_visualization.visualization import visualize_poses
 
 import utils
-from suturo_planning_manipulation.manipulation import Manipulation
-from suturo_planning_perception import perception
 from math import pi
 
 
