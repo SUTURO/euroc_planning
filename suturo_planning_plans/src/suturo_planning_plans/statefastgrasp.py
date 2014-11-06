7491f0a3e21d67400c732b17c7bdabc8c749a5bc
thocar <thocar@tzi.de>
2014-11-06 17:49:24 +0100

    fixes

--- suturo_planning_plans/src/suturo_planning_plans/statefastgrasp.py
+++ suturo_planning_plans/src/suturo_planning_plans/statefastgrasp.py
@@ -2,6 +2,7 @@ import copy
 from math import pi, sqrt
 import numpy
 from geometry_msgs.msg import Quaternion
+from rospy.service import ServiceException
 import smach
 import rospy
 from suturo_planning_manipulation.manipulation import PlanningException
@@ -16,7 +17,7 @@ from euroc_c2_msgs.srv import RequestNextObject
 class FastGrasp(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['objectGrasped', 'timeExpired', 'noPlanFound', 'graspingFailed',
-                                             'noObjectsLeft'],
+                                             'noObjectsLeft', 'fail'],
                              input_keys=['yaml', 'request_second_object'],
                              output_keys=['request_second_object'])
         self.__perceived_pose = 0
@@ -34,7 +35,14 @@ class FastGrasp(smach.State):
 
         # get the first perception
         rospy.logdebug('FastGrasp: call Perception Service')
-        resp = service("firstConveyorCall,cuboid")
+        for i in range(3):
+            try:
+                resp = service("firstConveyorCall,cuboid")
+            except ServiceException:
+                rospy.sleep(2)
+                if i == 2:
+                    return 'fail'
+
         if len(resp.objects) == 0 or not resp.objects[0].c_cuboid_success:
             rospy.logdebug('FastGrasp: objects empty or no cuboid 1')
             return -1
@@ -43,7 +51,14 @@ class FastGrasp(smach.State):
 
         # wait some time until the second perception
         rospy.sleep(Duration.from_sec(0.5))
-        resp = service("cuboid")
+        for i in range(3):
+            try:
+                resp = service("cuboid")
+            except ServiceException:
+                rospy.sleep(2)
+                if i == 2:
+                    return 'fail'
+
         if len(resp.objects) == 0 or not resp.objects[0].c_cuboid_success:
             rospy.logdebug('FastGrasp: objects empty or no cuboid 2')
             return -2
