d2a3bd4c35a3ba90fc5c68c5c8aa8f13c82c4fd5
thocar <thocar@tzi.de>
2014-11-06 15:09:25 +0100

    added more logging

--- suturo_planning_plans/src/suturo_planning_plans/statecamtodropzone.py
+++ suturo_planning_plans/src/suturo_planning_plans/statecamtodropzone.py
@@ -28,14 +28,21 @@ class CamToDropzone(smach.State):
         abort_after = 15
         then = int(time.time())
         now = int(time.time())
-        while now - then < abort_after and not listener.frameExists("drop_point"):
-            rospy.loginfo("wait for drop_point frame")
+        while not listener.frameExists("drop_point"):
+            if now - then < abort_after:
+                rospy.logdebug('CamToDropzone: drop_point frame not found!')
+                return 'fail'
+            rospy.loginfo("CamToDropzone: wait for drop_point frame")
             rospy.sleep(2.)
             now = int(time.time())
+
         then = int(time.time())
         now = int(time.time())
-        while now - then < abort_after and not listener.frameExists("mdl_middle"):
-            rospy.loginfo("wait for mdl_middle frame")
+        while not listener.frameExists("mdl_middle"):
+            if now - then < abort_after:
+                rospy.logdebug('CamToDropzone: mdl_middle frame not found!')
+                return 'fail'
+            rospy.loginfo("CamToDropzone: wait for mdl_middle frame")
             rospy.sleep(2.)
             now = int(time.time())
 
@@ -54,4 +61,5 @@ class CamToDropzone(smach.State):
                 return 'scanPoseReached'
             else:
                 if i == 2:
+                    rospy.logdebug('CamToDropzone: Cant move arm to scan_conveyor_pose')
                     return 'fail'
