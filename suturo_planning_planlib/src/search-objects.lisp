6a6e6f10c792619bf9a6dfdeafd7977881ab6f02
Andreas <aklots@tzi.de>
2015-01-02 12:20:28 +0100

    moved plan files

---
+++ suturo_planning_planlib/src/search-objects.lisp
@@ -0,0 +1,7 @@
+(in-package :planlib)
+
+(defun search-objects ()
+  (roslisp:with-ros-node ("two_ints_client")
+    (if (not (roslisp:wait-for-service "search_objects" 10))
+        (roslisp:ros-warn nil "Timed out waiting for service add_two_ints")  
+        (roslisp:call-service "search_objects" 'suturo_interface_msgs-srv:SearchObjects))))
