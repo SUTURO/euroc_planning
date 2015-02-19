4d72e17b711c94d143a42161886c8675bcd5b53f
Tobias Peters <topeters@tzi.de>
2015-02-19 15:49:06 +0000

    added service for grasp_object

--- suturo_planning_interface/src/suturo_planning_interface/grasp_object.py
+++ suturo_planning_interface/src/suturo_planning_interface/grasp_object.py
@@ -5,41 +5,31 @@ from suturo_planning_interface import utils
 from suturo_planning_manipulation.calc_grasp_position import calculate_grasp_position, get_pre_grasp
 from suturo_planning_visualization.visualization import visualize_poses
 from mathemagie import get_fingertip, subtract_point, magnitude
-from suturo_interface_msgs.srv import TaskDataServiceResponse, TaskDataService
+import suturo_interface_msgs.srv
 import time
 from suturo_perception_msgs.msg import EurocObject
 
 class GraspObject(object):
-    SERVICE_NAME = 'suturo/state/grasp_object'
+    SERVICE_NAME = 'suturo/manipulation/grasp_object'
 
     def __init__(self):
         self._create_service()
 
     def _create_service(self):
-        rospy.Service(self.SERVICE_NAME, TaskDataService, self.__handle_request)
+        rospy.Service(self.SERVICE_NAME, suturo_interface_msgs.srv.GraspObject, self.__handle_request)
 
     def __handle_request(self, req):
-        taskdata = req.taskdata
-        result = self.__grasp(taskdata)
-        return TaskDataServiceResponse(taskdata = taskdata, result = result)
+        result = self.__grasp(req.object, req.density)
+        return suturo_interface_msgs.srv.GraspObjectResponse(result)
 
-    def __grasp(self, taskdata):
+    def __grasp(self, collision_object, density):
         rospy.loginfo('Executing state GraspObject')
-        collision_object_name = taskdata.object_to_move.mpe_object.id
-        rospy.loginfo('Trying to grasp %s' % collision_object_name)
 
-        if taskdata.enable_movement:
-            move_to_func = utils.manipulation.move_arm_and_base_to
-            plan_to_func = utils.manipulation.plan_arm_and_base_to
-        else:
-            move_to_func = utils.manipulation.move_to
-            plan_to_func = utils.manipulation.plan_arm_to
+        move_to_func = utils.manipulation.move_to
+        plan_to_func = utils.manipulation.plan_arm_to
 
-        #get the collisionobject out of the planningscene
-        collision_object = utils.manipulation.get_planning_scene().get_collision_object(collision_object_name)
         if collision_object is None:
-            rospy.logwarn("Collision Object " + collision_object_name + " is not in planningscene.")
-            return 'objectNotInPlanningscene'
+            rospy.logwarn("Collision Object is not in planningscene.")
 
         rospy.logdebug("Grasping: " + str(collision_object))
 
@@ -48,13 +38,10 @@ class GraspObject(object):
         #filter out some invalid grasps
         grasp_positions = utils.manipulation.filter_low_poses(grasp_positions)
 
-        if not taskdata.enable_movement:
-            grasp_positions = utils.manipulation.filter_close_poses(grasp_positions)
+        grasp_positions = utils.manipulation.filter_close_poses(grasp_positions)
 
         if len(grasp_positions) == 0:
-            rospy.logwarn("No grasppositions found for " + collision_object_name)
-            taskdata.failed_object = taskdata.object_to_move
-            return 'noGraspPosition'
+            rospy.logwarn("No grasppositions found")
 
         #sort to try the best grasps first
         grasp_positions.sort(cmp=lambda x, y: utils.manipulation.cmp_pose_stamped(collision_object, x, y))
@@ -62,7 +49,7 @@ class GraspObject(object):
 
         utils.manipulation.open_gripper()
         grasp_positions = [utils.manipulation.transform_to(grasp) for grasp in grasp_positions]
-        utils.manipulation.blow_up_objects(do_not_blow_up_list=(collision_object_name))
+        utils.manipulation.blow_up_objects(do_not_blow_up_list=collision_object.id)
         for grasp in grasp_positions:
             plan_pre_grasp = plan_to_func(get_pre_grasp(grasp))
             if plan_pre_grasp is None:
@@ -70,7 +57,7 @@ class GraspObject(object):
 
             rospy.logdebug("Plan to pregraspposition found")
 
-            utils.manipulation.blow_up_objects(do_not_blow_up_list=("map", collision_object_name))
+            utils.manipulation.blow_up_objects(do_not_blow_up_list=("map", collision_object.id))
             plan_to_grasp = plan_to_func(grasp, start_state=utils.manipulation.get_end_state(plan_pre_grasp))
             if plan_to_grasp is None or not utils.manipulation.move_with_plan_to(plan_pre_grasp):
                 rospy.logdebug("Failed to move to Graspposition")
@@ -87,7 +74,7 @@ class GraspObject(object):
             if not utils.manipulation.close_gripper(collision_object, get_fingertip(utils.manipulation.transform_to(grasp))):
                 # taskdata.failed_object = taskdata.object_to_move
                 utils.manipulation.blow_down_objects()
-                return 'fail'
+                return False
             time.sleep(0.5)
             rospy.sleep(1.5)
 
@@ -98,38 +85,31 @@ class GraspObject(object):
                 rospy.logwarn("TF failed")
                 # taskdata.failed_object = taskdata.object_to_move
                 utils.manipulation.blow_down_objects()
-                return 'fail'
+                return False
 
-            density = 1
-            for obj in taskdata.yaml.objects:
-                if obj.name == collision_object_name:
-                    density = obj.primitive_densities[0]
-                    if isinf(density):
-                        rospy.logerr("Infinite density! WTF. setting density to 7850")
-                        density = 7850
+            if isinf(density):
+                rospy.logerr("Infinite density! WTF. setting density to 7850")
+                density = 7850
 
             utils.manipulation.load_object(utils.manipulation.calc_object_weight(collision_object, density),
                                            Vector3(com.point.x, com.point.y, com.point.z))
 
-            rospy.loginfo("grasped " + collision_object_name)
+            rospy.loginfo("grasped")
 
             #save grasp data for placing
-            taskdata.grasp = grasp
             fingertip = get_fingertip(grasp)
             fingertip_to_tcp = subtract_point(grasp.pose.position, fingertip.point)
-            taskdata.dist_to_obj = magnitude(fingertip_to_tcp)
 
             rospy.logdebug("lift object")
             the_pre_grasp = get_pre_grasp(grasp)
-            the_move_to_func = move_to_func(the_pre_grasp, do_not_blow_up_list=collision_object_name)
+            the_move_to_func = move_to_func(the_pre_grasp, do_not_blow_up_list=collision_object.id)
 
             if not the_move_to_func:
                 rospy.logwarn("couldnt lift object. continue anyway")
-            taskdata.failed_object = EurocObject()
-            return 'success'
+
+            return True
         rospy.logwarn("Grapsing failed.")
-        taskdata.failed_object = taskdata.object_to_move
         utils.manipulation.blow_down_objects()
-        return 'fail'
+        return False
 
 
